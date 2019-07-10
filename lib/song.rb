a799bb07de1c1df9cf8d74e2abedc8caef6d56b7
Brittany Javalera <brittjavalera@gmail.com>
2019-07-10 02:05:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def self.new_by_filename(filename)
     name = filename.split(" - ")[1]
     artist = filename.split(" - ")[0]
-    song = self.new(name)
+    new_song = self.new(name)
     song.artist_name = artist
     song
     end
